832d1735335b86838c1848c95b1e32cbc9c88337
Melkozerov Alexandr <gamesartwork@mail.ru>
2022-11-23 18:39:36 +0500

    initial commit

--- src/lr1/example12.java
+++ src/lr1/example12.java
@@ -1,4 +1,19 @@
 package lr1;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class example12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        {
+            System.out.println("введите возраст");
+        }
+        int yearsold = scanner.nextInt();
+
+        int yearsOld = Calendar.getInstance().get(Calendar.YEAR  )  - yearsold;
+        System.out.print(yearsOld + " год");
+        scanner.close();
+    }
 }
+
---
+++ src/lr1/example13.java
@@ -0,0 +1,13 @@
+package lr1;
+
+public class example13 {
+    public static void main(String[] args) {
+        int anum =100;
+        int bnum =25;
+        System.out.println("num: " + anum);
+        anum ++ + anum;
+
+        System.out.println("num + num " + bnum);
+    }
+}
+
