02a236d315fb71b25d61769b0ee298503de9334a
Melkozerov Alexandr <gamesartwork@mail.ru>
2022-11-26 22:42:46 +0500

    initial commit

--- src/lr1/example14.java
+++ src/lr1/example14.java
@@ -11,8 +11,14 @@ public class example14 {
         int num3 = num + 1;
         int num4 = num + num2 + num3;
         int num5 = (int) Math.pow(num4,2);
-        System.out.printf("%d %d %d %d\n", num,num2,num3,num5 );
+        System.out.println("Число: " + num);
+        System.out.println("Число на единицу меньше: " + num2);
+        System.out.println("Число на единицу больше: " + num3);
+        System.out.println("Квадрат суммы 3 чисел: " + num5);
         in.close();
 
     }
 }
+// Напишите программу, в которой пользователь вводит число,
+// а программой отображается последовательность из четырех чисел:
+// число, на единицу меньше введённого, введенное число и число, на единицу больше введенного.
--- src/lr1/example8.java
+++ src/lr1/example8.java
@@ -6,13 +6,13 @@ public class example8 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("input day" );
+        System.out.println("Введите день недели" );
         String day  = in.nextLine();
 
-        System.out.println("input date" );
+        System.out.println("Введите дату" );
         int date = in.nextInt();
 
-        System.out.println("input month" );
+        System.out.println("Введите номер месяца" );
         int month = in.nextInt();
 
         System.out.println("day: " + day);
@@ -21,3 +21,5 @@ public class example8 {
         in.close();
     }
 }
+//Напишите программу, в которой Пользователь последовательно вводит название текущего дня недели,
+// название месяца и дату (номер дня в месяце). Программа выводит сообщение о сегодняшней дате (день недели, дата, месяц).
