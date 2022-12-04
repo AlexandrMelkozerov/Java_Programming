a58d4a8c113a85a778a4fab9065b03187a0e5da9
Alexandr <gamesartwork@mail.ru>
2022-12-04 11:55:21 +0500

    initial commit

---
+++ src/lr2/example1.java
@@ -0,0 +1,19 @@
+package lr2;
+
+import java.util.Scanner;
+
+public class example1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("input number");
+        int n = in.nextInt();
+        System.out.println("The number " + (n % 3 !=0 ? "is not " : "") + "divsible by 3");
+        in.close();
+
+
+
+
+    }
+
+}
