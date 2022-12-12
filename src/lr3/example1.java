e8af3506fec301a4e58037a34527792d82113726
Alexandr <gamesartwork@mail.ru>
2022-12-12 19:15:36 +0500

    initial commit

--- src/lr3/example1.java
+++ src/lr3/example1.java
@@ -1,31 +1,44 @@
 package lr3;
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 public class example1 {
     public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            System.out.println("Введите размер массива: ");
-            int size = in.nextInt();
-            if (size > 0) {
-                Random random = new Random();
-                int[] ints = new int[size];
+        Scanner in = new Scanner(System.in);
 
-                for (int i = 0; i < ints.length; i++) {
-                    boolean iterator = true;
-                    int x = 0;
-                    while (iterator) {
-                        x = random.nextInt(100);
-                        if (x % 5 == 2) {
-                            iterator = false;
-                        }
-                    }
-                    ints[i] = x;
-                }
-                System.out.println(Arrays.toString(ints));
-            } else {
-                System.out.println("значение неккоректно ");
+        System.out.println("Введите целое число в диапазоне от 1 до 7");
+        int number = in.nextInt();
+        switch(number){
+            case 1:{
+                System.out.println("Число " + number + "  понедельник");
+                break;
             }
-
+            case 2:{
+                System.out.println("Число " + number + "  вторник");
+                break;
+            }
+            case 3:{
+                System.out.println("Число " + number + "  среда");
+                break;
+            }
+            case 4:{
+                System.out.println("Число " + number + "  четверг");
+                break;
+            }
+            case 5:{
+                System.out.println("Число " + number + "  пятница");
+                break;
+            }
+            case 6:{
+                System.out.println("Число " + number + "  суббота");
+                break;
+            }
+            case 7:{
+                System.out.println("Число " + number + "  воскресение");
+                break;
+            }
+            default:{
+                System.out.println("Введено некорректное значение");
+                break;
+            }
         }
+    }
 }
